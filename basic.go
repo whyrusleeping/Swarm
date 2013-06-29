491c441b5dd4c93011631637941c0d3637b050d9
Jeromy <jeromyj@gmail.com>
2013-06-28 22:44:56 -0700

    it builds\!\!\!

--- basic.go
+++ basic.go
@@ -12,7 +12,7 @@ type Basic struct {
 	X,Y int
 	x,y int
 	goalX, goalY int
-	queue *point
+	queue *Point
 	last_d int
 	pred_at_goal bool
 }
@@ -23,37 +23,38 @@ func (b *Basic) Move(g *Grid) Movement {
 
 	//Here is where we are going to do cool pathfinding stuff, or maybe in
 	//another function that gets passed in? 
+	return Movement{}
 }
 
 func (b *Basic) PredictPath(g *Grid) *PathQueue {
 	var direc *PathQueue
 
 	if b.AtPredictGoal() {
-		pred_at_goal = true
+		b.pred_at_goal = true
 		direc = NewPathQueue()
 	}
-	if b.last_d != UP && g.At(b.x, b.y - 1) == nil && !pred_at_goal {
+	if b.last_d != UP && g.At(b.x, b.y - 1) == nil && !b.pred_at_goal {
 		b.y--
 		b.last_d = UP
 		direc = b.PredictPath(g)
 	}
-	if b.last_d != DOWN && g.At(b.x, b.y + 1) == nil && !pred_at_goal {
+	if b.last_d != DOWN && g.At(b.x, b.y + 1) == nil && !b.pred_at_goal {
 		b.y++
 		b.last_d = DOWN
 		direc = b.PredictPath(g)
 	}
-	if b.last_d != LEFT && g.At(b.x - 1, b.y) == nil && !pred_at_goal {
+	if b.last_d != LEFT && g.At(b.x - 1, b.y) == nil && !b.pred_at_goal {
 		b.x--
 		b.last_d = LEFT
 		direc = b.PredictPath(g)
 	}
-	if b.last_d != RIGHT && g.At(b.x + 1, b.y) == nil && !pred_at_goal {
+	if b.last_d != RIGHT && g.At(b.x + 1, b.y) == nil && !b.pred_at_goal {
 		b.x++
 		b.last_d = RIGHT
 		direc = b.PredictPath(g)
 	}
-	if !b.AtDeadEnd() {
-		direc.pushBack(Point{x,y})
+	if !b.AtDeadEnd(g) {
+		direc.PushBack(Point{b.x,b.y})
 	}
 	return direc
 }
@@ -62,11 +63,11 @@ func (b *Basic) AtGoal() bool {
 	return b.X == b.goalX && b.Y == b.goalY
 }
 
-func (b *(Basic) AtPredictGoal() bool {
+func (b *Basic) AtPredictGoal() bool {
 	return b.x == b.goalX && b.y == b.goalY
 }
 
-func (b *Basic) AtDeadEnd() {
+func (b *Basic) AtDeadEnd(g *Grid) bool {
 	 count := 0
 
 	if b.last_d != UP && g.At(b.x, b.y-1) != nil {
