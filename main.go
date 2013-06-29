5211c4393aed9d760f49221c3b5b687ab5612d01
Jeromy <jeromyj@gmail.com>
2013-06-28 20:15:39 -0700

    move current run functions to simulation struct

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
 func main() {
-	//This whole function is temporary code, we want to make another struct that
-	//is the simulation struct to contain the run loop and other fun variables
-	runtime.GOMAXPROCS(4)
-	w := NewWindow(400,400)
-	g := NewGrid(400,400)
-	go func() {w.DrawChan <- g}()
-	w.Run()
+	s := NewSimulation(400,400)
+	s.Run()
 }
