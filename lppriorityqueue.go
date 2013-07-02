0938b795084a7343e5c1fc4b4ac7102ef4248b21
Jeromy <jeromyj@gmail.com>
2013-07-01 21:36:49 -0700

    add skeleton for pqueue

---
+++ lppriorityqueue.go
@@ -0,0 +1,17 @@
+package main
+
+type LPPriorityQueue struct {
+	arr []*LinkPoint
+}
+
+func NewPriorityQueue() *LPPriorityQueue {
+	return new(LPPriorityQueue)
+}
+
+func (lp *LPPriorityQueue) Push(p *LinkPoint) {
+
+}
+
+func (lp *LPPriorityQueue) PopMin() *LinkPoint {
+	return nil
+}
